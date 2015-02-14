65e7f6e3dca8f0879fbf4a6f05cd2fd2033c3c6e
Sriram Panyam <sri.panyam@gmail.com>
2015-02-14 01:24:46 -0800

    Initial layout

--- clients/web/app/js/controllers.js
+++ clients/web/app/js/controllers.js
@@ -2,10 +2,14 @@
 var slackApp = angular.module("slackApp", []);
 
 slackApp.controller("ChannelListCtrl", function($scope) {
+		$scope.currentTeam = {
+			"teamId": "team1",
+			"teamName": "Backbone"
+		};
 		$scope.channels = [
 			{"name": "Channel 1"},
 			{"name": "Channel 2"},
 			{"name": "Channel 3"},
 			{"name": "Channel 4"},
-		]
+		];
 });
