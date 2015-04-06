134012288920b07c8f76ea12f29b6539f2cd9451
Sriram Panyam <sri.panyam@gmail.com>
2015-04-06 08:28:58 -0700

    Using IsNil and Not(IsNil)

--- services/msg/tests/user_test.go
+++ services/msg/tests/user_test.go
@@ -7,7 +7,7 @@ import (
 
 func (s *TestSuite) TestCreateUserService(c *C) {
 	svc := s.serviceGroup.UserService
-	c.Assert(svc, Not(Equals), nil)
+	c.Assert(svc, Not(IsNil))
 }
 
 func (s *TestSuite) TestSaveUserEmptyId_ShouldCreateId(c *C) {
@@ -15,18 +15,18 @@ func (s *TestSuite) TestSaveUserEmptyId_ShouldCreateId(c *C) {
 	team, err := s.serviceGroup.TeamService.CreateTeam(1, "org", "team")
 	user := NewUser(0, "user1", team)
 	err = svc.SaveUser(user, false)
-	c.Assert(err, Equals, nil)
+	c.Assert(err, IsNil)
 	c.Assert(user.Id, Not(Equals), 0)
 	c.Assert(user.Username, Equals, "user1")
 
 	fetched_user, err := svc.GetUserById(user.Id)
 	c.Assert(fetched_user.Id, Equals, user.Id)
-	c.Assert(fetched_user.Team, Not(Equals), nil)
+	c.Assert(fetched_user.Team, Not(IsNil))
 	c.Assert(fetched_user.Team.Id, Equals, user.Team.Id)
 
 	fetched_user, _ = svc.GetUser("user1", team)
 	c.Assert(fetched_user.Id, Equals, user.Id)
-	c.Assert(fetched_user.Team, Not(Equals), nil)
+	c.Assert(fetched_user.Team, Not(IsNil))
 	c.Assert(fetched_user.Team.Id, Equals, user.Team.Id)
 }
 
@@ -34,9 +34,9 @@ func (s *TestSuite) TestGetUserFirstTime(c *C) {
 	svc := s.serviceGroup.UserService
 	team, _ := s.serviceGroup.TeamService.CreateTeam(1, "org", "team")
 	user, _ := svc.GetUserById(1)
-	c.Assert(user, Equals, (*User)(nil))
+	c.Assert(user, IsNil)
 	_, err := svc.GetUser("user1", team)
-	c.Assert(err, Not(Equals), nil)
+	c.Assert(err, Not(IsNil))
 }
 
 func (s *TestSuite) TestSaveUserNormal(c *C) {
@@ -45,7 +45,7 @@ func (s *TestSuite) TestSaveUserNormal(c *C) {
 	user := NewUser(0, "user1", team)
 	user.Object = Object{Id: 1}
 	err := svc.SaveUser(user, false)
-	c.Assert(err, Equals, nil)
+	c.Assert(err, IsNil)
 }
 
 func (s *TestSuite) TestCreateUserDuplicate(c *C) {
@@ -54,22 +54,22 @@ func (s *TestSuite) TestCreateUserDuplicate(c *C) {
 	user := User{Username: "user1", Team: team}
 
 	err := svc.SaveUser(&user, false)
-	c.Assert(err, Equals, nil)
+	c.Assert(err, IsNil)
 
 	// err = svc.SaveUser(&user, false)
-	// c.Assert(err, Not(Equals), nil)
+	// c.Assert(err, Not(IsNil))
 }
 
 /*
 func (s *TestSuite) TestSaveUserIdOrNameExists(c *C) {
 	svc := s.serviceGroup.UserService
 	user, err := svc.CreateUser("1", "user1")
-	c.Assert(err, Equals, nil)
+	c.Assert(err, IsNil)
 	user, err = svc.CreateUser("1", "user2")
-	c.Assert(err, Not(Equals), nil)
-	c.Assert(user, Equals, (*User)(nil))
+	c.Assert(err, Not(IsNil))
+	c.Assert(user, IsNil)
 	user, err = svc.CreateUser("2", "user1")
-	c.Assert(err, Not(Equals), nil)
-	c.Assert(user, Equals, (*User)(nil))
+	c.Assert(err, Not(IsNil))
+	c.Assert(user, IsNil)
 }
 */
